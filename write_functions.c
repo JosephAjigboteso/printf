c8cba5e89a7e2557ce2ff3eca9a056336c4735aa
calvary303 <nellyalbert303@gmail.com>
2022-10-18 17:01:35 -0700

    collab_one

---
+++ write_functions.c
@@ -0,0 +1,39 @@
+include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: 1 (Success)
+ */
+int _putchar(char c)
+{
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _puts -  writes a string to stdout
+ * @str: The string to print
+ * Return: 1 (Success)
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
